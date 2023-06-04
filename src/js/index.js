0dea3fab283f72c787062eba2027c6762ce9caf9
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2023-06-04 17:57:17 -0600

    Attempt to add menu item click event listener to swap menu screens using animations.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 const slideRightBack = 'slideRightBack';
 const menuItem = '.menu-list li';
 const active = 'active';
+const inactive = 'inactive';
 const menuScreen = '.menu-screen';
+const mainMenuId = '#main-menu';
+const mainMenu = document.querySelector(mainMenuId);
 const menuList = document.querySelectorAll(menuItem);
 let menuDelay = parseFloat(window.getComputedStyle(document.documentElement).getPropertyValue('--main-menu-delay'));
 
@@ -9,14 +12,21 @@ const setActive = (elm, selector) => {
    const activeElement = document.querySelector(`${selector}.${active}`);
    if (activeElement !== null) {
       activeElement.classList.remove(active);
+      activeElement.classList.add(inactive);
    }
    elm.classList.add(active);
 };
 
 menuList.forEach((item, itemIndex) => {
-   item.style.animation = `${slideRightBack} 0.8s linear ${(menuDelay + 1) + (itemIndex / 4)}s forwards`;
-   item.addEventListener('click', function() {
+   item.style.animationDelay = `${1 + (itemIndex / 4)}s`;
+   item.addEventListener('click', function () {
       const linkedElement = document.querySelector(this.dataset.link);
       setActive(linkedElement, menuScreen);
    });
 });
+
+
+setTimeout(function () {
+   setActive(mainMenu, menuScreen);
+   console.log('success');
+}, `${menuDelay * 1000}`);
