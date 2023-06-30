ce00dc4ff16ba48b85dbb2c9ea71f4cfcae4e13e
Savus <justin.fuzzygreenstuff@gmail.com>
2023-06-30 00:24:14 -0600

    Finish catching up with menu swapping functionality ready to merge.

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,26 @@
+// Element ID's 
 const mainMenuId = 'main-menu';
 const gameMenuId = 'games-menu';
 
+//Element queries
 const optionsMenuList = '.options-menu-list';
-
+const swapScreenSelector = '.swap-screen-container';
 
 const root = document.documentElement;
-
 const loadingScreenDelay = parseFloat(
    window
       .getComputedStyle(root)
       .getPropertyValue('--loading-screen-delay')
 );
 
+//CSS Classes
 const active = 'active';
+
+//Timing delays
 const menuDelay = 500;
+const menuLinksAnimationDelay = 2010;
 
-
+//Functions
 const setActive = (elm, selector, isDelayed = false, timingDelay = 300) => {
    const activeElement = document.querySelector(`${selector}.active`);
    if (activeElement !== null) {
@@ -29,17 +34,38 @@ const setActive = (elm, selector, isDelayed = false, timingDelay = 300) => {
    } else {
       elm.classList.add(active);
    }
+};
+
+const connectMenuLinks = (menuList) => {
+   menuList.addEventListener('click', function(e) {
+      const dataLink = e.target.dataset.link;
+      const elementToSwap = document.getElementById(dataLink);
+      setActive(elementToSwap, swapScreenSelector, true, menuDelay);
+   });
+};
+
+const setLinkAnimationDelay = (menuList) => {
+   const menuChildren = Array.from(menuList.children);
+   menuChildren.forEach((listItem, index) => {
+      //set animation delay
+      setTimeout(() => {
+         listItem.style.animationDelay = `${index * 0.18}s`;
+      },menuLinksAnimationDelay);
+   });
 }
 
+//Elements
 const mainMenu = document.getElementById(mainMenuId);
 const gameMenu = document.getElementById(gameMenuId);
-
 const optionsMenuLists = document.querySelectorAll(optionsMenuList);
 
+//Initialization logic
+setTimeout(function(){
+   setActive(mainMenu, '#loading-screen-container');
+}, loadingScreenDelay * 1200)
+
 optionsMenuLists.forEach((menuList) => {
-   Array.from(menuList.children).forEach((listItem, index) => {
-      setTimeout(() => {
-         listItem.classList.add(active);
-      }, index * 200);
-   })
-})
+   connectMenuLinks(menuList);
+   setLinkAnimationDelay(menuList);
+});
+
