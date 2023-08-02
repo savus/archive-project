300bd22bc762afe95957cdd73cf9389ebc38368d
Savus <justin.fuzzygreenstuff@gmail.com>
2023-08-01 23:28:32 -0600

    create visual grid and align it properly

--- battleship-src/battleship.js
+++ battleship-src/battleship.js
@@ -2,71 +2,152 @@ const alphabetList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const player1Id = 'player-1';
 const player1 = document.getElementById(player1Id);
 const boardSymbols = {
-   'empty': ' -- ',
-   'hit': 'X',
-   'miss': 'O',
-   'occupied': 'H'
-};
-
+   'empty' : '-',
+   'hit' : 'X',
+   'occupied' : 'O'
+}
 const createGrid = (gridSize) => {
    const grid = [];
-   for (let i = 0; i < gridSize; i++) {
+   for (let i = 0; i <= gridSize; i++) {
       grid[i] = [];
-      for (let j = 0; j < gridSize; j++) {
-         grid[i][j] = {x: j, y: i, status: "empty"};
+      for (let j = 0; j <= gridSize; j++) {
+         grid[i][j] = {x: j, y: i, status: 'empty'};
       }
    }
    return grid;
-};
-
-const grid = createGrid(10);
-
-const createHeader = (grid) => {
-   let header = `<div class='board-header'> `;
-   for (let i = 0; i < grid.length; i++) {
-      header += ` | ${alphabetList[i]}`;
-   }
-   header += '</div>';
-   return header;
 }
 
-const drawGrid = (element, grid) => {
-   let child = '<div>';
-   const header = createHeader(grid);
-   child += ` ${header} `;
+const grid1 = createGrid(25);
+
+const createHeaders = (grid) => {
+   const headerContainer = document.createElement('div');
+   headerContainer.classList.add('cell-row');
+   const firstCell = document.createElement('div');
+   firstCell.innerText = ' - ';
+   headerContainer.appendChild(firstCell);   
    for (let i = 0; i < grid.length; i++) {
-      let gridRow = `<div>${i + 1} |`;
-      for (let cell of grid[i]) {
-         gridRow += ` ${boardSymbols[cell.status]} |`;
+      const cell = document.createElement('div');
+      cell.innerText = `${alphabetList[i]}`;
+      headerContainer.appendChild(cell);
+   }
+   return headerContainer;
+}
+
+
+const drawGrid = (parent, grid) => {
+   const header = createHeaders(grid);
+   parent.appendChild(header);
+
+   for (let i = 0; i <= grid.length; i++) {
+      const cellRow = document.createElement('div');
+      const firstCell = document.createElement('div');
+      cellRow.classList.add('cell-row');
+      firstCell.innerText = i;
+      cellRow.appendChild(firstCell);
+
+      for (const cell of grid[i]) {
+         const cellDiv = document.createElement('div');
+         cellDiv.innerText = boardSymbols[cell.status];
+         cellRow.appendChild(cellDiv);
       }
-      gridRow += '</div>';
-      child += gridRow;
+      parent.appendChild(cellRow);
    }
-   child += '</div>';
-   element.innerHTML = child;
-};
 
-drawGrid(player1, grid);
-
-const shipData = [
-   {name: 'destroyer', length: 2}, 
-   {name: 'submarine', length: 3}, 
-   {name: 'cruiser', length: 3},
-   {name: 'battleship', length: 4}, 
-   {name: 'carrier', length: 5}
-];
-
-class Ship {
-   constructor(name, length) {
-      this.name = name;
-      this.length = length;
-   }
 }
 
-const createShips = () => {
-   return shipData.map((stat) => new Ship(stat.name, stat.length));
-}
+drawGrid(player1, grid1);
 
-const player1Ships = createShips();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const alphabetList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// const player1Id = 'player-1';
+// const player1 = document.getElementById(player1Id);
+// const boardSymbols = {
+//    'empty': ' -- ',
+//    'hit': 'X',
+//    'miss': 'O',
+//    'occupied': 'H'
+// };
+
+// const createGrid = (gridSize) => {
+//    const grid = [];
+//    for (let i = 0; i < gridSize; i++) {
+//       grid[i] = [];
+//       for (let j = 0; j < gridSize; j++) {
+//          grid[i][j] = {x: j, y: i, status: "empty"};
+//       }
+//    }
+//    return grid;
+// };
+
+// const grid = createGrid(10);
+
+// const createHeader = (grid) => {
+//    let header = `<div class='board-header'> `;
+//    for (let i = 0; i < grid.length; i++) {
+//       header += ` | ${alphabetList[i]}`;
+//    }
+//    header += '</div>';
+//    return header;
+// }
+
+// const drawGrid = (element, grid) => {
+//    let child = '<div>';
+//    const header = createHeader(grid);
+//    child += ` ${header} `;
+//    for (let i = 0; i < grid.length; i++) {
+//       let gridRow = `<div>${i + 1} |`;
+//       for (let cell of grid[i]) {
+//          gridRow += ` ${boardSymbols[cell.status]} |`;
+//       }
+//       gridRow += '</div>';
+//       child += gridRow;
+//    }
+//    child += '</div>';
+//    element.innerHTML = child;
+// };
+
+// drawGrid(player1, grid);
+
+// const shipData = [
+//    {name: 'destroyer', length: 2}, 
+//    {name: 'submarine', length: 3}, 
+//    {name: 'cruiser', length: 3},
+//    {name: 'battleship', length: 4}, 
+//    {name: 'carrier', length: 5}
+// ];
+
+// class Ship {
+//    constructor(name, length) {
+//       this.name = name;
+//       this.length = length;
+//    }
+// }
+
+// const createShips = () => {
+//    return shipData.map((stat) => new Ship(stat.name, stat.length));
+// }
+
+// const player1Ships = createShips();
 
 
