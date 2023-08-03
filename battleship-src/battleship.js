8b5124849fe0e20dcc7d2dc2554baaea64704092
Savus <justin.fuzzygreenstuff@gmail.com>
2023-08-02 20:12:51 -0600

    create functions that check available spaces of grid and place occupied ship tiles.

--- battleship-src/battleship.js
+++ battleship-src/battleship.js
@@ -1,87 +1,111 @@
-const alphabetList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const player1Id = 'player-1';
+const alphabetList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const player1Id = "player-1";
 const player1 = document.getElementById(player1Id);
 const boardSymbols = {
-   'empty' : '-',
-   'occupied' : 'H',
-   'hit' : 'X',
-   'miss' : 'O'
+  empty: "-",
+  occupied: "H",
+  hit: "X",
+  miss: "O",
 };
 
-
-
 const createGrid = (gridSize) => {
-   const grid = [];
-   for (let i = 0; i <= gridSize; i++) {
-      grid[i] = [];
-      for (let j = 0; j <= gridSize; j++) {
-         grid[i][j] = {x: j, y: i, status: 'empty'};
-      }
-   }
-   return grid;
-}
+  const grid = [];
+  for (let i = 0; i <= gridSize; i++) {
+    grid[i] = [];
+    for (let j = 0; j <= gridSize; j++) {
+      grid[i][j] = { x: j, y: i, status: "empty" };
+    }
+  }
+  return grid;
+};
 
 const createHeaders = (grid) => {
-   const headerRow = document.createElement('div');
-   headerRow.classList.add('cell-row');
+  const headerRow = document.createElement("div");
+  headerRow.classList.add("cell-row");
 
-   const firstCell = document.createElement('div');
-   firstCell.innerText = '-';
+  const firstCell = document.createElement("div");
+  firstCell.innerText = "-";
 
-   headerRow.appendChild(firstCell);
+  headerRow.appendChild(firstCell);
 
-   for (let i = 0; i <= grid.length; i++) {
-      const cell = document.createElement('div');
-      cell.innerText = alphabetList[i];
-      headerRow.appendChild(cell);
-   }
-   
-   return headerRow;
+  for (let i = 0; i < grid.length; i++) {
+    const cell = document.createElement("div");
+    cell.innerText = alphabetList[i];
+    headerRow.appendChild(cell);
+  }
+
+  return headerRow;
 };
 
 const drawGrid = (parent, grid) => {
-   const header = createHeaders(grid);
-   parent.appendChild(header);
-   for (let i = 0; i < grid.length; i++) {
-      const cellRow = document.createElement('div');
-      cellRow.classList.add('cell-row');
-      
-      const firstCell = document.createElement('div');
-      firstCell.innerText = i;
-      cellRow.appendChild(firstCell);
+  const header = createHeaders(grid);
+  parent.appendChild(header);
+  for (let i = 0; i < grid.length; i++) {
+    const cellRow = document.createElement("div");
+    cellRow.classList.add("cell-row");
 
-      for (const cell of grid[i]) {
-         const cellDiv = document.createElement('div');
-         cellDiv.innerText = boardSymbols[cell.status];
-         cellRow.appendChild(cellDiv);
-      }
+    const firstCell = document.createElement("div");
+    firstCell.innerText = i;
+    cellRow.appendChild(firstCell);
 
-      parent.appendChild(cellRow);
-   }
-}
+    for (const cell of grid[i]) {
+      const cellDiv = document.createElement("div");
+      cellDiv.innerText = boardSymbols[cell.status];
+      cellRow.appendChild(cellDiv);
+    }
 
-const grid1 = createGrid(alphabetList.length - 2);
+    parent.appendChild(cellRow);
+  }
+};
+
+const eraseGrid = (parent) => {
+  parent.innerHTML = "";
+};
+
+const resetGrid = (parent, grid) => {
+  eraseGrid(parent);
+  drawGrid(parent, grid);
+};
+
+const randomizeCoords = (gridSize) => {
+  const coords = [
+    Math.floor(Math.random() * gridSize),
+    Math.floor(Math.random() * gridSize),
+  ];
+  return coords;
+};
+
+const getStartingPoint = (grid) => {
+  const [col, row] = randomizeCoords(grid.length);
+  if (grid[col][row].status !== "empty") return getStartingPoint(grid);
+  return [col, row];
+};
+
+const checkAvailableSpaces = (grid, shipLength) => {
+  const [col, row] = getStartingPoint(grid);
+  for (let i = 0; i < shipLength; i++) {
+    if (
+      grid[col][row + i].status !== "empty" ||
+      grid[col][row + i].x + 1 >= grid.length
+    )
+      return checkAvailableSpaces(grid, shipLength);
+  }
+  return [col, row];
+};
+
+const placeShips = (parent, grid, shipLength) => {
+  const [col, row] = checkAvailableSpaces(grid, shipLength);
+  for (let i = 0; i < shipLength; i++) {
+    grid[col][row + i].status = "occupied";
+  }
+  resetGrid(parent, grid);
+};
+
+const grid1 = createGrid(10);
 
 drawGrid(player1, grid1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (let i = 0; i < 5; i++) placeShips(player1, grid1, 5);
 
 // const alphabetList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // const player1Id = 'player-1';
@@ -134,10 +158,10 @@ drawGrid(player1, grid1);
 // drawGrid(player1, grid);
 
 // const shipData = [
-//    {name: 'destroyer', length: 2}, 
-//    {name: 'submarine', length: 3}, 
+//    {name: 'destroyer', length: 2},
+//    {name: 'submarine', length: 3},
 //    {name: 'cruiser', length: 3},
-//    {name: 'battleship', length: 4}, 
+//    {name: 'battleship', length: 4},
 //    {name: 'carrier', length: 5}
 // ];
 
@@ -153,5 +177,3 @@ drawGrid(player1, grid1);
 // }
 
 // const player1Ships = createShips();
-
-
