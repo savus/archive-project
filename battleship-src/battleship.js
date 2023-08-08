ffc36ae682617f727a5ebf6742f0b6d6d6ceda7e
Savus <justin.fuzzygreenstuff@gmail.com>
2023-08-08 11:28:47 -0600

    add message element, input field, and button to html document. add functions that receive user's input, convert it to viable board locations, determine whether the space chosen has been a hit, miss, or has already been previously chosen. relay combat results to user.

--- battleship-src/battleship.js
+++ battleship-src/battleship.js
@@ -30,7 +30,7 @@ const createHeaders = (grid) => {
 
   for (let i = 0; i < grid.length; i++) {
     const cell = document.createElement("div");
-    cell.innerText = alphabetList[i];
+    cell.innerText = i;
     headerRow.appendChild(cell);
   }
 
@@ -45,7 +45,7 @@ const drawGrid = (parent, grid) => {
     cellRow.classList.add("cell-row");
 
     const firstCell = document.createElement("div");
-    firstCell.innerText = i;
+    firstCell.innerText = alphabetList[i];
     cellRow.appendChild(firstCell);
 
     for (const cell of grid[i]) {
@@ -128,12 +128,63 @@ shipData.forEach((data) => ships.push(new Ship(data.name, data.length)));
 
 let numberOfShips = ships.length;
 
+const player1Message = document.getElementById('player-1-grid-message');
+player1Message.innerText = 'Mew';
+const player1Input = document.getElementById('player-1-grid-prompt');
+player1Input.setAttribute('placeholder', 'mew');
+const promptButton = document.getElementById('button-prompt');
+
+
+const attackResults = (grid, y, x) => grid[y][x].status;
+
+const relayResults = (results) => {
+  const messageOutcomes = {
+    'empty': () => 'You missed!',
+    'occupied': () => 'That\'s a hit!',
+    'hit': () => 'You struck this area before! Miss!',
+    'miss': () => 'You missed this area before! Miss!'
+  }
+  return messageOutcomes[results]();
+}
+
+const convertResults = (str) => {
+  const convertedStr = str.toLowerCase();
+  const firstCoord = alphabetList.toLowerCase().indexOf(convertedStr[0]);
+  const secondCoord = convertedStr[convertedStr.length - 1];
+  return [firstCoord, secondCoord];
+}
+
+const commenceAttack = (coords) => {
+  const grid = grid1;
+  const parent = player1;
+  const [alteredCol, alteredRow] = convertResults(coords);
+  const results = attackResults(grid, alteredCol, alteredRow);  
+  player1Message.innerText = relayResults(results);
+
+  const outcomes = {
+    'empty' : () => 'miss',
+    'occupied' : () => 'hit',
+    'miss' : () => 'miss',
+    'hit' : () => 'hit'
+  }
+
+  const newBoardSymbol = outcomes[results]();
+
+  grid[alteredCol][alteredRow].status = newBoardSymbol;
+
+  redrawGrid(parent, grid);
+}
+
+promptButton.addEventListener('click', function() {
+  const inputValue = player1Input.value.replaceAll(' ', '');
+  commenceAttack(inputValue);
+});
 
 const grid1 = createGrid(10);
 
 drawGrid(player1, grid1);
 
-ships.forEach((ship) => placeShip(player1, grid1, ship))
+ships.forEach((ship) => placeShip(player1, grid1, ship));
 // const alphabetList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // const player1Id = 'player-1';
 // const player1 = document.getElementById(player1Id);
