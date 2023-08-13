5317c5013757df409ebb967add72790b534d9cf5
Savus <justin.fuzzygreenstuff@gmail.com>
2023-08-13 01:49:17 -0600

    create computer player and board with ship pieces

--- battleship-src/battleship.js
+++ battleship-src/battleship.js
@@ -1,12 +1,15 @@
 const alphabetList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const player1Id = "player-1";
 const player1 = document.getElementById(player1Id);
+const computerId = "computer";
+const computer = document.getElementById(computerId);
 const boardSymbols = {
   empty: "-",
   occupied: "H",
   hit: "X",
   miss: "O",
 };
+const gridSize = 10;
 
 const createGrid = (gridSize) => {
   const grid = [];
@@ -85,11 +88,16 @@ const shipData = [
   { name: "carrier", length: 5 },
 ];
 
-const ships = [];
+const playerShips = [];
+const computerShips = [];
 
-shipData.forEach((data) => ships.push(new Ship(data.name, data.length)));
+shipData.forEach((data) => {
+  playerShips.push(new Ship(data.name, data.length));
+  computerShips.push(new Ship(data.name, data.length));
+});
 
-let numberOfShips = ships.length;
+let numberOfplayerShips = playerShips.length;
+let numberOfComputerShips = computerShips.length;
 
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
@@ -137,15 +145,15 @@ const placeShip = (parent, grid, ship) => {
 };
 
 const player1Message = document.getElementById("player-1-grid-message");
-player1Message.innerText = "Mew";
+player1Message.innerText = "Please choose a tile to attack (ex. A-5)";
 const player1Input = document.getElementById("player-1-grid-prompt");
-player1Input.setAttribute("placeholder", "mew");
-const promptButton = document.getElementById("button-prompt");
+player1Input.setAttribute("placeholder", "Type here");
+const promptButton = document.getElementById("player-1-button-prompt");
 
 const damageShip = (ship, damagedPiece) => {
   if (ship.lives - 1 === 0) {
-    numberOfShips--;
-    player1Message.innerText = `You sunk a ${ship.name}, ${numberOfShips} remaining!`;
+    numberOfplayerShips--;
+    player1Message.innerText = `You sunk a ${ship.name}, ${numberOfplayerShips} remaining!`;
   } else {
     ship.lives--;
     damagedPiece.status = "damaged";
@@ -163,16 +171,6 @@ const checkIfHit = (coordsY, coordsX, shipList) => {
       }
     });
   });
-  // for (let i = 0; i < shipList.length; i++) {
-  //   for (let j = 0; j < shipList[i].locations.length; j++) {
-  //     if (shipList[i].locations[j].x === coordsX && shipList[i].locations[j].y === coordsY) {
-  //       console.log('hit');
-  //       shipList[i].locations[j].status = 'damaged';
-  //       shipList[i].lives--;
-  //       return shipList[i];
-  //     }
-  //   }
-  // }
 };
 
 const attackResults = (grid, y, x) => grid[y][x].status;
@@ -209,90 +207,23 @@ const commenceAttack = (grid, parent, coords) => {
   const newBoardSymbol = outcomes[results]();
 
   grid[alteredCol][alteredRow].status = newBoardSymbol;
-  console.log(alteredCol, alteredRow, ships);
 
   redrawGrid(parent, grid);
 
-  checkIfHit(alteredCol, alteredRow, ships);
+  checkIfHit(alteredCol, alteredRow, playerShips);
 };
 
 promptButton.addEventListener("click", function () {
   const inputValue = player1Input.value.replaceAll(" ", "");
-  commenceAttack(grid1, player1, inputValue);
+  commenceAttack(playerGrid, player1, inputValue);
 });
 
-const grid1 = createGrid(10);
+const playerGrid = createGrid(gridSize);
+const computerGrid = createGrid(gridSize);
 
-drawGrid(player1, grid1);
+drawGrid(player1, playerGrid);
+drawGrid(computer, computerGrid);
 
-ships.forEach((ship) => placeShip(player1, grid1, ship));
+playerShips.forEach((ship) => placeShip(player1, playerGrid, ship));
+computerShips.forEach((ship) => placeShip(computer, computerGrid, ship));
 // const alphabetList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// const player1Id = 'player-1';
-// const player1 = document.getElementById(player1Id);
-// const boardSymbols = {
-//    'empty': ' -- ',
-//    'hit': 'X',
-//    'miss': 'O',
-//    'occupied': 'H'
-// };
-
-// const createGrid = (gridSize) => {
-//    const grid = [];
-//    for (let i = 0; i < gridSize; i++) {
-//       grid[i] = [];
-//       for (let j = 0; j < gridSize; j++) {
-//          grid[i][j] = {x: j, y: i, status: "empty"};
-//       }
-//    }
-//    return grid;
-// };
-
-// const grid = createGrid(10);
-
-// const createHeader = (grid) => {
-//    let header = `<div class='board-header'> `;
-//    for (let i = 0; i < grid.length; i++) {
-//       header += ` | ${alphabetList[i]}`;
-//    }
-//    header += '</div>';
-//    return header;
-// }
-
-// const drawGrid = (element, grid) => {
-//    let child = '<div>';
-//    const header = createHeader(grid);
-//    child += ` ${header} `;
-//    for (let i = 0; i < grid.length; i++) {
-//       let gridRow = `<div>${i + 1} |`;
-//       for (let cell of grid[i]) {
-//          gridRow += ` ${boardSymbols[cell.status]} |`;
-//       }
-//       gridRow += '</div>';
-//       child += gridRow;
-//    }
-//    child += '</div>';
-//    element.innerHTML = child;
-// };
-
-// drawGrid(player1, grid);
-
-// const shipData = [
-//    {name: 'destroyer', length: 2},
-//    {name: 'submarine', length: 3},
-//    {name: 'cruiser', length: 3},
-//    {name: 'battleship', length: 4},
-//    {name: 'carrier', length: 5}
-// ];
-
-// class Ship {
-//    constructor(name, length) {
-//       this.name = name;
-//       this.length = length;
-//    }
-// }
-
-// const createShips = () => {
-//    return shipData.map((stat) => new Ship(stat.name, stat.length));
-// }
-
-// const player1Ships = createShips();
